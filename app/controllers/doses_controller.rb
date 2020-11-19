1d017296daa99f22583719a5393ead2fcb5ae129
mathieu fauvette <fauvettemathieu.mf@gmail.com>
2020-11-19 15:53:36 +0100

    aller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,23 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.save
+    redirect_to doses_path
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to doses_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :coktails do
+  root to: "cocktails#home"
+  resources :cocktails do
     resources :doses, only: [ :new, :create , :destroy]
   end
 end
